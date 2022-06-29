7de92dfd0092e020b6c858a08c83f1d45060993b
Nicholas_Huang713 <nhuang713@gmail.com>
2022-06-29 00:37:33 -0700

    test api on frontend

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+
 function App() {
+  const [apiMsg, setApiMsg] = useState("");
+  const baseUrl = "http://localhost:5000";
+
+  useEffect(() => {
+    const checkApiRoute = async () => {
+      try {
+        const resp = await axios(`${baseUrl}/`);
+        console.log(resp.data.msg)
+        setApiMsg(resp.data.msg);
+      } catch (e) {
+        setApiMsg("Oops, something went wrong...")
+      }
+    }
+    checkApiRoute();
+  }, []);
+
   return (
     <div>
-      Pokemon Cards App!
+      <h1>Pokemon Cards!</h1>
+      {apiMsg}
     </div>
   );
 }
