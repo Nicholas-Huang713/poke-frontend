905035aff11c12040d789222912cf2c3af11d310
Nicholas_Huang713 <nhuang713@gmail.com>
2022-07-27 10:40:17 -0700

    update text

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <div>
-      <h1>Hello Pokemon Cards!</h1>
+      <h1>Hello!</h1>
       {/* <ul>
         {userList.length > 0 && userList.map((user) => (
           <li>
